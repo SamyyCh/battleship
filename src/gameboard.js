83ef9b999557b09844d7bfcaf9e0dd5e71b6849b
MustafaTv <samychattali@me.com>
2024-03-20 17:21:18 -0400

    Perfected Place Ship Function

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,22 +1,32 @@
-// import Ship from './ship';
+const Ship = require('./ship')
 
 class Gameboard {
     constructor() {
-        this.height = 10,
-        this.width = this.height,
-        this.size = this.height * this.width,
-        this.map = []
+        this.height = 10;
+        this.width = this.height;
+        this.size = this.height * this.width;
+        this.map = [];
+
+        for (let i = 0; i < this.height; i++) {
+            this.map.push(Array(this.width).fill(null));
+        }
     }
 
-    placeShip() {
-        const xCord = [];
-        const yCord = [];
-        return this.position = [xCord, yCord]
+    placeShip(x, y, length) {
+        const ship = new Ship(length);
+
+        if (x + length > this.width || y + length > this.height) {
+            throw new Error("Out of bounds");
+        }
+
+        for (let i = 0; i < length; i++) {
+            this.map[x + i][y] = ship;
+        }
     }
 
-    receiveAttack(xCord, yCord) {
+    receiveAttack(x, y) {
         
     }
 }
 
-module.exports = Gameboard;
+module.exports = Gameboard
