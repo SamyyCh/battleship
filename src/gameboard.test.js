2f9af47b1da4d039e5bfc518c1d29d6b65414298
MustafaTv <samychattali@me.com>
2024-03-20 17:21:42 -0400

    Tested placeShip Functions

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,8 +1,25 @@
 const Gameboard = require('./gameboard.js');
+const Ship = require('./ship.js');
 
 describe("map", () => {
     it('should be 100', () => {
         const map = new Gameboard();
         expect(map.size).toBe(100)
+    })
+})
+
+describe("placeShip", () => {
+    it('should throw an error', () => {
+        const map = new Gameboard();
+        expect(() => map.placeShip(6, 1, 5)).toThrow(Error)
+    })
+
+    it.only('should place a ship', () => {
+        const map = new Gameboard();
+        map.placeShip(2, 3, 3);
+
+        for (let i = 0; i < 3; i++) {
+            expect(map.map[2 + i][3]).toBeInstanceOf(Ship)
+        }
     })
 })
