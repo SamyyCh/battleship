2df9c0c8c4cee57072f9d63349523b941bd6d8bf
MustafaTv <samychattali@me.com>
2024-03-21 18:20:47 -0400

    Tested receiveAttack function

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -14,12 +14,28 @@ describe("placeShip", () => {
         expect(() => map.placeShip(6, 1, 5)).toThrow(Error)
     })
 
-    it.only('should place a ship', () => {
+    it('should place a ship', () => {
         const map = new Gameboard();
         map.placeShip(2, 3, 3);
 
         for (let i = 0; i < 3; i++) {
             expect(map.map[2 + i][3]).toBeInstanceOf(Ship)
         }
+    })
+})
+
+describe("receiveAttack", () => {
+    it("receive and hit boat", () => {
+        const board = new Gameboard();
+        board.placeShip(0, 0, 3);
+        board.receiveAttack(0, 0);
+        expect(board.map[0][0].hits).toBe(1);
+    })
+
+    it("record a miss", () => {
+        const board = new Gameboard();
+        board.placeShip(0, 0, 3);
+        board.receiveAttack(1, 1)
+        expect(board.receiveAttack(1, 1)).toEqual([[1, 1]]);
     })
 })
